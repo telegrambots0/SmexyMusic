ac3c7452cd098192c162c44294f2a32750ca2db3
telegrambots0 <146921432+telegrambots0@users.noreply.github.com>
2024-03-14 14:09:51 +0530

    Update __init__.py

--- VenomX/__init__.py
+++ VenomX/__init__.py
@@ -1,4 +1,4 @@
-from VenomX.core.bot import Ayush
+from VenomX.core.bot import SmexyOP
 from VenomX.core.dir import dirr
 from VenomX.core.git import git
 from VenomX.core.userbot import Userbot
@@ -11,7 +11,7 @@ git()
 dbb()
 heroku()
 
-app = Ayush()
+app = SmexyOP()
 userbot = Userbot()
 
 
