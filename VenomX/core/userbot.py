8641ec02346168d5c1d194f6e2464c9946eb5bcd
telegrambots0 <146921432+telegrambots0@users.noreply.github.com>
2024-03-14 13:04:46 +0530

    Update userbot.py

--- VenomX/core/userbot.py
+++ VenomX/core/userbot.py
@@ -54,8 +54,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("VenomOwners")
-                await self.one.join_chat("Venom_Chatz")
+                await self.one.join_chat("SmexyStore")
+                await self.one.join_chat("SmexyStoreChat")
             except:
                 pass
             assistants.append(1)
@@ -83,8 +83,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("VenomOwners")
-                await self.two.join_chat("Venom_Chatz")
+                await self.two.join_chat("SmexyStore")
+                await self.two.join_chat("SmexyStoreChat")
             except:
                 pass
             assistants.append(2)
@@ -112,8 +112,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("VenomOwners")
-                await self.three.join_chat("Venom_Chatz")
+                await self.three.join_chat("SmexyStore")
+                await self.three.join_chat("SmexyStoreChat")
             except:
                 pass
             assistants.append(3)
@@ -141,8 +141,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("VenomOwners")
-                await self.four.join_chat("Venom_Chatz")
+                await self.four.join_chat("SmexyStore")
+                await self.four.join_chat("SmexyStoreChat")
             except:
                 pass
             assistants.append(4)
@@ -170,8 +170,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("VenomOwners")
-                await self.five.join_chat("Venom_Chatz")
+                await self.five.join_chat("SmexyStore")
+                await self.five.join_chat("SmexyStoreChat")
             except:
                 pass
             assistants.append(5)
